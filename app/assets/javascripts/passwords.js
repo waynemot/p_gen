4f7e34df48e33a00977fa6c0baa6f5396e481e55
Wayne Motycka <wmotycka3@unl.edu>
2022-05-20 15:27:32 -0500

    add app dir @ stop point

--- app/assets/javascripts/passwords.js
+++ app/assets/javascripts/passwords.js
@@ -4,10 +4,18 @@
 
 $("#results").change(
     function() {
+        console.log("Results changed....")
         var res_set = $(this).split(/\r?\n/);
         var res_out = [];
-        if( res_set.size() > 0) {
-
+        console.log("set size: "+res_set.length)
+        if( res_set.length > 0) {
+            for(var i = 0; i < res_set.length; i++) {
+                res_out = res_set[i]+" strength: "+zxcvbn(res_set[i]);
+            }
+            console.log("result out size: "+res_out.length)
+            if (res_out.length > 0) {
+                alert("Strengths:\n"+res_out.join("\n"));
+            }
         }
     }
 )
